8ad7690fd966025e6ae120e40da88c14efb67279
Robert Mutua <robert@shofrica.com>
2021-04-14 01:05:24 +0200

    changed home to shop

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ function Header() {
     <header className="md:absolute md:left-0 md:top-0 w-full z-10">
       <div className="py-3 lg:py-5 flex items-center">
         <Link href="/">
-          <a title="Return to ChopChop">Home</a>
+          <a title="Return to ChopChop">Shop</a>
         </Link>
         <span className="pr-1">,</span>
         <CartSummary />
